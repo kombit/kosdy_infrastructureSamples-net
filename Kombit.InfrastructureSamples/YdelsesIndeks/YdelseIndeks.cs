f9fef4940ce8f0998a2f6895b6d487f45f2ddb7a
steffenlomholt <steffen@lomholt.biz>
2024-10-19 14:57:26 +0200

    Initial commit

---
+++ Kombit.InfrastructureSamples/YdelsesIndeks/YdelseIndeks.cs
@@ -0,0 +1,82 @@
+﻿using Kombit.InfrastructureSamples.YdelseIndeksService;
+using Kombit.InfrastructureSamples.Token;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.IdentityModel.Tokens;
+
+namespace Kombit.InfrastructureSamples.YdelsesIndeks
+{
+    internal class YdelseIndeks
+    {
+        private SecurityToken token;
+        private YdelseIndeksPortType port;
+
+        #region Port and token helper methods
+
+        /// <summary>
+        /// The Port property used to send requests. Creates a new port only if it doesn't already exist, or the token has expired
+        /// </summary>
+        private YdelseIndeksPortType Port
+        {
+            get
+            {
+                if (port == null || TokenFetcher.IsTokenExpired(token))
+                {
+                    port = CreatePort();
+                }
+
+                return port;
+            }
+            set
+            {
+                port = value;
+            }
+        }
+        /// <summary>
+        /// Creates the port by getting a token, setting the endpoint and loading the certificates.
+        /// </summary>
+        /// <returns></returns>
+        private YdelseIndeksPortType CreatePort()
+        {
+            //TODO Changeg ConfigVariables.SagDokServiceEntityId to ConfigVariables.YdelseServiceEntityId
+            token = TokenFetcher.IssueToken(ConfigVariables.SagDokServiceEntityId);
+            YdelseIndeksPortTypeClient client = new YdelseIndeksPortTypeClient();
+
+            EndpointIdentity identity = EndpointIdentity.CreateDnsIdentity(ConfigVariables.ServiceCertificateAlias);
+            EndpointAddress endpointAddress = new EndpointAddress(client.Endpoint.ListenUri, identity);
+            client.Endpoint.Address = endpointAddress;
+            var certificate = CertificateLoader.LoadCertificate(
+                ConfigVariables.ClientCertificateStoreName,
+                ConfigVariables.ClientCertificateStoreLocation,
+                ConfigVariables.ClientCertificateThumbprint
+            );
+            client.ClientCredentials.ClientCertificate.Certificate = certificate;
+
+            // This sets the MINIMUM level. Since the request header should not be signed, we set it to none.
+            client.Endpoint.Contract.ProtectionLevel = ProtectionLevel.None;
+
+            return client.ChannelFactory.CreateChannelWithIssuedToken(token);
+        }
+
+        /// <summary>
+        /// Creates the request header which is simply a random UUID
+        /// </summary>
+        private RequestHeaderType RequestHeader
+        {
+            get
+            {
+                return new RequestHeaderType()
+                {
+                    TransactionUUID = Guid.NewGuid().ToString()
+                };
+            }
+        }
+
+        #endregion
+    }
+}
