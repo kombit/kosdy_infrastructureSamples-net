157e13f2e8a8590128d8e626bdb896a224c7d25d
Steffen Lomholt Hansen <steffen@lomholt.biz>
2025-05-23 20:15:22 +0200

    Code example for fremsoeg completed

---
+++ Kombit.InfrastructureSamples/YdelsesIndeks/XmlSaver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Kombit.InfrastructureSamples.UnitTests
+{
+    public class XmlSaver
+    {
+        public static void SaveRequestAsXml(YdelseIndeksService.fremsoegRequest request, string filePath)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(YdelseIndeksService.fremsoegRequest));
+
+            using (FileStream fs = new FileStream(filePath, FileMode.Create))
+            {
+                serializer.Serialize(fs, request);
+            }
+        }
+    }
+}
